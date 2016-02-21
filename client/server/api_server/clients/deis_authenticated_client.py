e733046fb5e507d1a4d0e8d2c1129b202a984b65
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-21 18:59:57 -0500

    Added a login function to DeisClient Created the class DeisAuthenticatedClient added unit tests for these

---
+++ client/server/api_server/clients/deis_authenticated_client.py
@@ -0,0 +1,17 @@
+from api_server.clients.deis_client import DeisClient
+
+
+class DeisAuthenticatedClient(DeisClient):
+    """The Deis client for API that requires authentication
+    """
+
+    def __init__(self, deis_url, token):
+        """Create a new ``DeisAuthenticatedClient``.
+
+        @type deis_url: str
+
+        @type token: str
+            Authentication token for this user.
+        """
+        super(self.__class__, self).__init__(deis_url)
+        self.token = token
