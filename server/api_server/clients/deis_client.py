2e9b71aea9a32df4392ce57f9532337648446ed5
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-21 18:59:57 -0500

    Added a login function to DeisClient Created the class DeisAuthenticatedClient added unit tests for these

--- server/api_server/clients/deis_client.py
+++ server/api_server/clients/deis_client.py
@@ -4,7 +4,7 @@ import urlparse
 from api_server.clients.deis_client_errors import DeisClientResponseError
 
 
-class DeisClient:
+class DeisClient(object):
 
     def __init__(self, deis_url):
         """Create a new ``DeisClient``.
@@ -21,6 +21,8 @@ class DeisClient:
 
         @type path: str
             The extra http path to be appended to the deis URL
+
+        @rtype: requests.Response
         """
         return requests.request(method, urlparse.urljoin(self.deis_url, path), **kwargs)
 
@@ -30,6 +32,8 @@ class DeisClient:
         @type username: str
         @type password: str
         @type email: str
+
+        @raise e: DeisClientResponseError
         """
         resp = self._request('POST', 'v1/auth/register/', json={
             "username": username,
@@ -38,3 +42,25 @@ class DeisClient:
         })
         if resp.status_code != 201:
             raise DeisClientResponseError(resp)
+
+    def login(self, username, password):
+        """Login to Deis and return an authenticated client.
+
+        @type username: str
+        @type password: str
+
+        @rtype: DeisAuthenticatedClient
+
+        @raise e: DeisClientResponseError
+        """
+        # this avoids circular imports
+        from api_server.clients.deis_authenticated_client import DeisAuthenticatedClient
+
+        resp = self._request('POST', 'v1/auth/login/', json={
+            "username": username,
+            "password": password
+        })
+        if resp.status_code != 201:
+            raise DeisClientResponseError(resp)
+        token = resp.json()['token']
+        return DeisAuthenticatedClient(self.deis_url, token)
--- server/api_server/tests/unit_tests/clients/test_deis_client.py
+++ server/api_server/tests/unit_tests/clients/test_deis_client.py
@@ -60,3 +60,31 @@ def test_register_failure(deis_client, fake_deis_url, username, password, email)
         fake_deis_url, 'v1/auth/register/'), status=400)
     with pytest.raises(DeisClientResponseError):
         deis_client.register(username, password, email)
+
+
+@responses.activate
+def test_login_success(deis_client, fake_deis_url, username, password):
+    """
+    @type deis_client: DeisClient
+    @type fake_deis_url: str
+    @type username: str
+    @type password: str
+    """
+    responses.add(responses.POST, urlparse.urljoin(
+        fake_deis_url, 'v1/auth/login/'), status=201, json={"token": "sometoken"})
+    auth_client = deis_client.login(username, password)
+    assert auth_client.token == 'sometoken'
+
+
+@responses.activate
+def test_login_failure(deis_client, fake_deis_url, username, password):
+    """
+    @type deis_client: DeisClient
+    @type fake_deis_url: str
+    @type username: str
+    @type password: str
+    """
+    responses.add(responses.POST, urlparse.urljoin(
+        fake_deis_url, 'v1/auth/login/'), status=400)
+    with pytest.raises(DeisClientResponseError):
+        deis_client.login(username, password)
