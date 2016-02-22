511cf1ce340bac63dd6ba1e418811817dd56dea6
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-21 20:03:13 -0500

    added integration tests for logging in and app creation

---
+++ client/server/api_server/tests/integration_tests/clients/test_deis_authenticated_client.py
@@ -0,0 +1,19 @@
+import pytest
+
+from api_server.clients.deis_client_errors import DeisClientResponseError
+
+
+@pytest.fixture
+def deis_authenticated_client(deis_client, username, password, email):
+    deis_client.register(username, password, email)
+    return deis_client.login(username, password)
+
+
+def test_application(deis_authenticated_client, app_id):
+    deis_authenticated_client.create_application(app_id)
+
+    with pytest.raises(DeisClientResponseError):
+        deis_authenticated_client.create_application(app_id)
+
+    ids = deis_authenticated_client.get_all_applications()
+    assert ids == [app_id]
