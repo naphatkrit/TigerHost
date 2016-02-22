301cfa4c7183c91d8444c46cb0ec61c976853add
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-22 09:23:56 -0500

    refactored tests to only test a single failure case with _request_and_raise function

--- server/api_server/tests/unit_tests/clients/test_deis_authenticated_client.py
+++ server/api_server/tests/unit_tests/clients/test_deis_authenticated_client.py
@@ -3,7 +3,6 @@ import pytest
 import urlparse
 
 from api_server.clients.deis_authenticated_client import DeisAuthenticatedClient
-from api_server.clients.deis_client_errors import DeisClientResponseError
 
 
 @pytest.fixture
@@ -27,18 +26,6 @@ def test_get_all_applications_success(deis_authenticated_client, fake_deis_url):
 
 
 @responses.activate
-def test_get_all_applications_failure(deis_authenticated_client, fake_deis_url):
-    """
-    @type deis_authenticated_client: DeisAuthenticatedClient
-    @type fake_deis_url: str
-    """
-    responses.add(responses.GET, urlparse.urljoin(
-        fake_deis_url, 'v1/apps'), status=400)
-    with pytest.raises(DeisClientResponseError):
-        deis_authenticated_client.get_all_applications()
-
-
-@responses.activate
 def test_create_application_success(deis_authenticated_client, fake_deis_url):
     """
     @type deis_authenticated_client: DeisAuthenticatedClient
@@ -47,15 +34,3 @@ def test_create_application_success(deis_authenticated_client, fake_deis_url):
     responses.add(responses.POST, urlparse.urljoin(
         fake_deis_url, 'v1/apps/'), status=201)
     deis_authenticated_client.create_application('testid')
-
-
-@responses.activate
-def test_create_application_failure(deis_authenticated_client, fake_deis_url):
-    """
-    @type deis_authenticated_client: DeisAuthenticatedClient
-    @type fake_deis_url: str
-    """
-    responses.add(responses.POST, urlparse.urljoin(
-        fake_deis_url, 'v1/apps/'), status=400)
-    with pytest.raises(DeisClientResponseError):
-        deis_authenticated_client.create_application('testid')
