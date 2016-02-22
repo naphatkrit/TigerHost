a443e51c95e78a7fde57f14918cf9e2fa4a89cc3
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-22 18:27:04 -0500

    [server] added CAS authentication

--- client/server/api_server/views.py
+++ client/server/api_server/views.py
@@ -1,3 +1,8 @@
 # from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required
+def token_generate_page(request):
+    return HttpResponse('testing')
