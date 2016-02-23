8e9c8056c12ce5b8c6e06c5d77e7f1ed166b1ffb
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-23 09:46:18 -0500

    [wsse] added a decorator to check if the function contains wsse token.

---
+++ server/wsse/tests/unit_tests/test_utils.py
@@ -0,0 +1,28 @@
+import pytest
+
+from wsse.utils import parse_wsse_header
+
+
+_wsse_header = '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"'''
+
+
+def test_parse_wsse_header_success():
+    username, digest, nonce, timestamp = parse_wsse_header(_wsse_header)
+    assert username == 'bob'
+    assert digest == 'quR/EWLAV4xLf9Zqyw4pDmfV9OY='
+    assert nonce == 'd36e316282959a9ed4c89851497a717f'
+    assert timestamp == '2003-12-15T14:43:07Z'
+
+
+@pytest.mark.parametrize('wsse_header', [
+    '''UsernameToken PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"''',
+
+    '''UsernameToken Username="bob", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"''',
+
+    '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Created="2003-12-15T14:43:07Z"''',
+
+    '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f"'''
+])
+def test_parse_wsse_header_success_failure(wsse_header):
+    with pytest.raises(ValueError):
+        parse_wsse_header(wsse_header)
---
+++ server/wsse/utils.py
@@ -0,0 +1,20 @@
+def parse_wsse_header(wsse_header):
+    """Take a wsse header and parses it, returning a tuple of username,
+    password digest, nonce, and timestamp.
+
+    @type wsse_header: str
+
+    @rtype: tuple
+        (username, digest, nonce, timestamp) - all str
+
+    @raises e: ValueError
+        if the header is not in a valid format
+    """
+    try:
+        username = wsse_header.split('Username="')[1].split('"')[0]
+        digest = wsse_header.split('PasswordDigest="')[1].split('"')[0]
+        nonce = wsse_header.split('Nonce="')[1].split('"')[0]
+        timestamp = wsse_header.split('Created="')[1].split('"')[0]
+    except IndexError:
+        raise ValueError
+    return username, digest, nonce, timestamp
