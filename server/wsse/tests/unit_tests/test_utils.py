68ae2bd99a2a07ded922cc96f12843232a772dd2
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-23 14:23:39 -0500

    [wsse] refactored tests

--- server/wsse/tests/unit_tests/test_utils.py
+++ server/wsse/tests/unit_tests/test_utils.py
@@ -1,37 +1,25 @@
 import pytest
 
+from wsse.test_data import valid_wsse_headers, invalid_wsse_headers, valid_wsse_digests
 from wsse.utils import parse_wsse_header, wsse_digest
 
 
-_wsse_header = '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"'''
+@pytest.mark.parametrize('wsse_header,correct', valid_wsse_headers)
+def test_parse_wsse_header_success(wsse_header, correct):
+    username, digest, nonce, timestamp = parse_wsse_header(wsse_header)
+    assert username == correct['username']
+    assert digest == correct['digest']
+    assert nonce == correct['nonce']
+    assert timestamp == correct['timestamp']
 
 
-def test_parse_wsse_header_success():
-    username, digest, nonce, timestamp = parse_wsse_header(_wsse_header)
-    assert username == 'bob'
-    assert digest == 'quR/EWLAV4xLf9Zqyw4pDmfV9OY='
-    assert nonce == 'd36e316282959a9ed4c89851497a717f'
-    assert timestamp == '2003-12-15T14:43:07Z'
-
-
-@pytest.mark.parametrize('wsse_header', [
-    '''UsernameToken PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"''',
-
-    '''UsernameToken Username="bob", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"''',
-
-    '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Created="2003-12-15T14:43:07Z"''',
-
-    '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f"'''
-])
+@pytest.mark.parametrize('wsse_header', invalid_wsse_headers)
 def test_parse_wsse_header_success_failure(wsse_header):
     with pytest.raises(ValueError):
         parse_wsse_header(wsse_header)
 
 
-@pytest.mark.parametrize('secret,nonce,timestamp,correct', [
-    ('taadtaadpstcsm', 'd36e316282959a9ed4c89851497a717f',
-     '2003-12-15T14:43:07Z', 'quR/EWLAV4xLf9Zqyw4pDmfV9OY=')
-])
-def test_wsse_digest(secret, nonce, timestamp, correct):
-    digest = wsse_digest(secret, nonce, timestamp)
-    assert digest == correct
+@pytest.mark.parametrize('data', valid_wsse_digests)
+def test_wsse_digest(data):
+    digest = wsse_digest(data['secret'], data['nonce'], data['timestamp'])
+    assert digest == data['digest']
