263fb31a1aaa6b11d546e2cd29cf3003552faf4a
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-23 13:51:32 -0500

    [wsse] digest code

--- server/wsse/tests/unit_tests/test_utils.py
+++ server/wsse/tests/unit_tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from wsse.utils import parse_wsse_header
+from wsse.utils import parse_wsse_header, wsse_digest
 
 
 _wsse_header = '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"'''
@@ -26,3 +26,12 @@ def test_parse_wsse_header_success():
 def test_parse_wsse_header_success_failure(wsse_header):
     with pytest.raises(ValueError):
         parse_wsse_header(wsse_header)
+
+
+@pytest.mark.parametrize('secret,nonce,timestamp,correct', [
+    ('taadtaadpstcsm', 'd36e316282959a9ed4c89851497a717f',
+     '2003-12-15T14:43:07Z', 'quR/EWLAV4xLf9Zqyw4pDmfV9OY=')
+])
+def test_wsse_digest(secret, nonce, timestamp, correct):
+    digest = wsse_digest(secret, nonce, timestamp)
+    assert digest == correct
