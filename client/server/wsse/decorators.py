0fc68e8799319889eddc4d246d3cdf0777f56de1
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-23 09:46:18 -0500

    [wsse] added a decorator to check if the function contains wsse token.

---
+++ client/server/wsse/decorators.py
@@ -0,0 +1,48 @@
+from functools import wraps
+
+from django.contrib import auth
+from django.http import HttpResponse
+from django.utils.decorators import available_attrs
+
+from wsse.utils import parse_wsse_header
+
+
+def check_wsse_token(view_func):
+    """A decorator that, if the user is not already authenticated, attempts
+    to authenticate the user using wsse token.
+
+    If the user is already authenticated, this doesn't do anything.
+
+    If the user is not successfully authenticated, this returns a HTTP 401
+    """
+    def create_401_response():
+        response = HttpResponse('Unauthorized', status=401)
+        response['WWW-Authenticate'] = 'WSSE realm="api", profile="UsernameToken"'
+        return response
+
+    @wraps(view_func, assigned=available_attrs(view_func))
+    def wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return view_func(request, *args, **kwargs)
+
+        if 'HTTP_AUTHORIZATION' not in request.META or (request.META['HTTP_AUTHORIZATION'] != 'WSSE profile="UsernameToken"'):
+            return create_401_response()
+
+        if 'HTTP_X_WSSE' not in request.META:
+            return create_401_response()
+
+        wsse_header = request.META['HTTP_X_WSSE']
+        try:
+            username, digest, nonce, timestamp = parse_wsse_header(
+                wsse_header)
+        except ValueError:
+            return create_401_response()
+
+        user = auth.authenticate(
+            username=username, password_digest=digest, nonce=nonce, timestamp=timestamp)
+        if user is None:
+            return create_401_response()
+        auth.login(request, user)
+        return view_func(request, *args, **kwargs)
+
+    return wrapped_view
---
+++ client/server/wsse/tests/unit_tests/test_decorators.py
@@ -0,0 +1,96 @@
+import mock
+import pytest
+
+from django.contrib.auth.models import User, AnonymousUser
+from django.http import HttpRequest, HttpResponse
+
+from wsse.decorators import check_wsse_token
+
+
+@check_wsse_token
+def _dummy_view(request):
+    assert request.user.is_authenticated()
+    return HttpResponse()
+
+
+@pytest.fixture(scope='function')
+def mock_user():
+    user = mock.Mock(spec=User)
+    user.is_authenticated.return_value = True
+    return user
+
+
+@pytest.fixture(scope='function')
+def mock_request():
+    request = mock.Mock(spec=HttpRequest)
+    request.user = AnonymousUser()
+    request.META = {}
+    request.META['HTTP_AUTHORIZATION'] = 'WSSE profile="UsernameToken"'
+    request.META['HTTP_X_WSSE'] = '''UsernameToken Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"'''
+    return request
+
+
+def mock_login(request, user):
+    request.user = user
+
+
+def test_check_wsse_token_success(mock_request, mock_user):
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login):
+        mock_authenticate.return_value = mock_user
+        response = _dummy_view(mock_request)
+    assert response.status_code == 200
+
+
+def test_check_wsse_token_success_pre_authenticated(mock_request, mock_user):
+    mock_request.user = mock_user
+    response = _dummy_view(mock_request)
+    assert response.status_code == 200
+
+
+def test_check_wsse_token_no_authorization(mock_request, mock_user):
+    mock_request.META.pop('HTTP_AUTHORIZATION', None)
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login):
+        mock_authenticate.return_value = mock_user
+        response = _dummy_view(mock_request)
+    assert response.status_code == 401
+    assert response[
+        'WWW-Authenticate'] == 'WSSE realm="api", profile="UsernameToken"'
+
+
+def test_check_wsse_token_wrong_authorization_method(mock_request, mock_user):
+    mock_request.META['HTTP_AUTHORIZATION'] = 'some other method'
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login):
+        mock_authenticate.return_value = mock_user
+        response = _dummy_view(mock_request)
+    assert response.status_code == 401
+    assert response[
+        'WWW-Authenticate'] == 'WSSE realm="api", profile="UsernameToken"'
+
+
+def test_check_wsse_token_no_wsse_header(mock_request, mock_user):
+    mock_request.META.pop('HTTP_X_WSSE', None)
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login):
+        mock_authenticate.return_value = mock_user
+        response = _dummy_view(mock_request)
+    assert response.status_code == 401
+    assert response[
+        'WWW-Authenticate'] == 'WSSE realm="api", profile="UsernameToken"'
+
+
+def test_check_wsse_token_invalid_wsse_header(mock_request, mock_user):
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login), mock.patch('wsse.decorators.parse_wsse_header') as mock_parse:
+        mock_parse.side_effect = ValueError
+        mock_authenticate.return_value = mock_user
+        response = _dummy_view(mock_request)
+    assert response.status_code == 401
+    assert response[
+        'WWW-Authenticate'] == 'WSSE realm="api", profile="UsernameToken"'
+
+
+def test_check_wsse_token_not_authenticated(mock_request, mock_user):
+    with mock.patch('django.contrib.auth.authenticate') as mock_authenticate, mock.patch('django.contrib.auth.login', new=mock_login):
+        mock_authenticate.return_value = None
+        response = _dummy_view(mock_request)
+    assert response.status_code == 401
+    assert response[
+        'WWW-Authenticate'] == 'WSSE realm="api", profile="UsernameToken"'
