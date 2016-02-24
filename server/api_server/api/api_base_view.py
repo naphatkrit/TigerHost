71f9d8bb9f00e6e012bc4b14b582f4fbc2e2fd4b
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-24 10:25:30 -0500

    [server] handle Deis Client Response Error

--- server/api_server/api/api_base_view.py
+++ server/api_server/api/api_base_view.py
@@ -1,10 +1,25 @@
+from functools import wraps
+
 from django.conf import settings
 from django.http import JsonResponse
+from django.utils.decorators import available_attrs, method_decorator
 from django.views.generic import View
 
+from api_server.clients.deis_client_errors import DeisClientResponseError
 from api_server.clients.deis_client import DeisClient
 
 
+def _handle_deis_client_response_error(f):
+    @wraps(f, assigned=available_attrs(f))
+    def wrapped_view(request, *args, **kwargs):
+        try:
+            return f(request, *args, **kwargs)
+        except DeisClientResponseError as e:
+            return JsonResponse(e.response.json(), status=e.response.status_code)
+    return wrapped_view
+
+
+@method_decorator(_handle_deis_client_response_error, 'dispatch')
 class ApiBaseView(View):
     deis_client = DeisClient(settings.DEIS_URL)
 
