682547daf80f708b0c709570490fead1d03a9ea9
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-24 10:44:05 -0500

    [server] app create api endpoint

--- server/api_server/api/apps_api_view.py
+++ server/api_server/api/apps_api_view.py
@@ -1,3 +1,5 @@
+import json
+
 from django.utils.decorators import method_decorator
 
 from api_server.api.api_base_view import ApiBaseView
@@ -19,3 +21,22 @@ class AppsApiView(ApiBaseView):
 
         app_ids = auth_client.get_all_applications()
         return self.respond_multiple(app_ids)
+
+    def post(self, request):
+        """Create a new application.
+
+        The body of the request should be a JSON with the following format:
+        {
+            'id': 'app_id_here'
+        }
+
+        @type request: django.http.HttpRequest
+
+        @rtype: django.http.HttpResponse
+        """
+        app_id = json.loads(request.body)['id']
+        auth_client, _ = self.deis_client.login_or_register(
+            request.user.username, request.user.profile.get_paas_password(), request.user.email)
+
+        auth_client.create_application(app_id)
+        return self.respond()
