ba942263b6c3c27d5d5af95105e770574f37a78d
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-25 09:22:48 -0500

    [server] API endpoint for app collaborators

---
+++ client/server/api_server/api/app_collaborators_api_view.py
@@ -0,0 +1,45 @@
+import json
+
+from django.utils.decorators import method_decorator
+
+from api_server.api.api_base_view import ApiBaseView
+from wsse.decorators import check_wsse_token
+
+
+@method_decorator(check_wsse_token, 'dispatch')
+class AppCollaboratorsApiView(ApiBaseView):
+
+    def get(self, request, app_id):
+        """Get the list of usernames who are collaborators on
+        this app. This does NOT include the app owner.
+
+        @type request: django.http.HttpRequest
+        @type app_id: str
+
+        @rtype: django.http.HttpResponse
+        """
+        auth_client, _ = self.deis_client.login_or_register(
+            request.user.username, request.user.profile.get_paas_password(), request.user.email)
+
+        users = auth_client.get_application_collaborators()
+        return self.respond_multiple(users)
+
+    def post(self, request, app_id):
+        """Add a collaborator to this app.
+
+        The body of the request must be a JSON with the format
+        {
+            "username": "userid",
+        }
+
+        @type request: django.http.HttpRequest
+        @type app_id: str
+
+        @rtype: django.http.HttpResponse
+        """
+        username = json.loads(request.body)['username']
+        auth_client, _ = self.deis_client.login_or_register(
+            request.user.username, request.user.profile.get_paas_password(), request.user.email)
+
+        auth_client.add_application_collaborator(app_id, username)
+        return self.respond()
---
+++ client/server/api_server/tests/unit_tests/api/test_app_collaborators_api_view.py
@@ -0,0 +1,34 @@
+import json
+import mock
+import pytest
+
+
+@pytest.mark.django_db
+def test_GET(client, http_headers, mock_deis_authenticated_client):
+    """
+    @type client: django.test.Client
+    @type http_headers: dict
+    @type mock_deis_authenticated_client: mock.Mock
+    """
+    users = ['username1', 'username2']
+    mock_deis_authenticated_client.get_application_collaborators.return_value = users
+    with mock.patch('api_server.api.api_base_view.ApiBaseView.deis_client') as mock_deis_client:
+        mock_deis_client.login_or_register.return_value = mock_deis_authenticated_client, False
+        resp = client.get('/api/v1/apps/testid/collaborators/', **http_headers)
+    assert resp.status_code == 200
+    assert set(resp.json()['results']) == set(users)
+
+
+@pytest.mark.django_db
+def test_POST(client, http_headers, mock_deis_authenticated_client):
+    """
+    @type client: django.test.Client
+    @type http_headers: dict
+    @type mock_deis_authenticated_client: mock.Mock
+    """
+    user = 'usernam1'
+    with mock.patch('api_server.api.api_base_view.ApiBaseView.deis_client') as mock_deis_client:
+        mock_deis_client.login_or_register.return_value = mock_deis_authenticated_client, False
+        resp = client.post('/api/v1/apps/testid/collaborators/', data=json.dumps({'username': user}), content_type='application/json', **http_headers)
+    assert resp.status_code == 204
+    mock_deis_authenticated_client.add_application_collaborator.assert_called_once_with('testid', user)
