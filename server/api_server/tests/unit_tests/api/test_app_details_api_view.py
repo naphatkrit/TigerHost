bb9b6dc69a14dae2bb35478ad4087c2361a8c2d4
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-25 09:05:24 -0500

    [server] API endpoint for app owner

--- server/api_server/tests/unit_tests/api/test_app_details_api_view.py
+++ server/api_server/tests/unit_tests/api/test_app_details_api_view.py
@@ -1,5 +1,43 @@
+import json
 import mock
 import pytest
+
+
+@pytest.mark.django_db
+def test_GET(client, http_headers, mock_deis_authenticated_client):
+    """
+    @type client: django.test.Client
+    @type http_headers: dict
+    @type mock_deis_authenticated_client: mock.Mock
+    """
+    owner = 'userid'
+    mock_deis_authenticated_client.get_application_owner.return_value = owner
+    with mock.patch('api_server.api.api_base_view.ApiBaseView.deis_client') as mock_deis_client:
+        mock_deis_client.login_or_register.return_value = mock_deis_authenticated_client, False
+        resp = client.get('/api/v1/apps/testid/', **http_headers)
+    assert resp.status_code == 200
+    assert resp.json() == {
+        'owner': owner,
+    }
+
+
+@pytest.mark.django_db
+def test_POST(client, http_headers, mock_deis_authenticated_client):
+    """
+    @type client: django.test.Client
+    @type http_headers: dict
+    @type mock_deis_authenticated_client: mock.Mock
+    """
+    owner = 'userid'
+    with mock.patch('api_server.api.api_base_view.ApiBaseView.deis_client') as mock_deis_client:
+        mock_deis_client.login_or_register.return_value = mock_deis_authenticated_client, False
+        resp = client.post('/api/v1/apps/testid/',
+                           data=json.dumps({'owner': owner}),
+                           content_type='application/json',
+                           **http_headers)
+    assert resp.status_code == 204
+    mock_deis_authenticated_client.set_application_owner.assert_called_once_with(
+        'testid', owner)
 
 
 @pytest.mark.django_db
