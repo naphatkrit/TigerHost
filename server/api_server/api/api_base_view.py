6a0facbff8594016138b97eee6b2d31165b64fdc
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-26 09:31:33 -0500

    [server] added decorator to handle exceptions in API view

--- server/api_server/api/api_base_view.py
+++ server/api_server/api/api_base_view.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import available_attrs, method_decorator
 from django.views.generic import View
 
-from api_server.clients.deis_client_errors import DeisClientResponseError
+from api_server.clients.deis_client_errors import DeisClientResponseError, DeisClientError, DeisClientTimeoutError
 from api_server.clients.deis_client import DeisClient
 
 
@@ -19,6 +19,21 @@ def _handle_deis_client_response_error(f):
     return wrapped_view
 
 
+def _handle_error(exception, status, message=None):
+    def decorator(f):
+        @wraps(f, assigned=available_attrs(f))
+        def wrapped_view(request, *args, **kwargs):
+            try:
+                return f(request, *args, **kwargs)
+            except exception as e:
+                message1 = message if message is not None else '{}'.format(e)
+                return JsonResponse({'error': message1}, status=status)
+        return wrapped_view
+    return decorator
+
+
+@method_decorator(_handle_error(DeisClientError, status=500), 'dispatch')
+@method_decorator(_handle_error(DeisClientTimeoutError, status=500, message='PaaS server timeout'), 'dispatch')
 @method_decorator(_handle_deis_client_response_error, 'dispatch')
 class ApiBaseView(View):
     deis_client = DeisClient(settings.DEIS_URL)
