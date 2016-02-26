6d937f4bef0e0ed30d999a3a9b6119010f30e06d
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-26 15:39:45 -0500

    [client] app management commands

--- client/client/tigerhost/commands/apps.py
+++ client/client/tigerhost/commands/apps.py
@@ -16,3 +16,28 @@ def list_apps(api_client):
     apps = api_client.get_all_applications()
     for app in apps:
         click.echo(app)
+
+
+@click.command()
+@click.argument('name')
+@decorators.print_markers
+@decorators.catch_exception(ApiClientResponseError)
+@decorators.pass_api_client
+def create_app(api_client, name):
+    """Create a new app with the specified NAME.
+    """
+    api_client.create_application(name)
+    click.echo('App {} created.'.format(name))
+
+
+@click.command()
+@click.option('--app', '-a', help='Optionally specify which app to work with. Defaults to the current app.')
+@decorators.print_markers
+@decorators.catch_exception(ApiClientResponseError)
+@decorators.pass_api_client
+def destroy_app(api_client, app):
+    """Delete the current application.
+    """
+    assert app
+    api_client.delete_application(app)
+    click.echo('App {} destroyed.'.format(app))
