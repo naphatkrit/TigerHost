feefe261c3751a9a1222aecaac992be46265ca4e
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-29 14:15:00 -0500

    make unit tests use a sqlite database

--- settings/test.py
+++ settings/test.py
@@ -1,3 +1,8 @@
 from settings.dev import *  # NOQA
 
 ROOT_URLCONF = 'test_urls'
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
+}
