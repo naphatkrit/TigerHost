de7bac207c7f0a7fd15c977576859bb3f0c9ae82
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-29 09:33:36 -0500

    [client] modified integration tests make_app fixture to not create redundant folder

--- client/tests/integration_tests/commands/conftest.py
+++ client/tests/integration_tests/commands/conftest.py
@@ -1,8 +1,6 @@
-import os
 import pytest
 
 from tigerhost.entry import entry
-from tigerhost.utils import contextmanagers
 
 
 @pytest.fixture(scope='function')
@@ -11,11 +9,7 @@ def logged_in_user(runner, username, api_key, ensure_private_dir):
     assert result.exit_code == 0
 
 
-@pytest.yield_fixture(scope='function')
-def make_app(runner, logged_in_user, app_id):
-    os.makedirs(app_id)
-    with contextmanagers.chdir(app_id):
-        assert not os.system('git init')
-        result = runner.invoke(entry, ['create', app_id])
-        assert result.exit_code == 0
-        yield
+@pytest.fixture(scope='function')
+def make_app(runner, make_git_repo, logged_in_user, app_id):
+    result = runner.invoke(entry, ['create', app_id])
+    assert result.exit_code == 0
