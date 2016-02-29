298ed104128b716dac37937dfcf8b7a642136c5c
Naphat Sanguansin <naphat.krit@gmail.com>
2016-02-29 09:59:28 -0500

    [client] command to add git remote

--- client/tigerhost/entry.py
+++ client/tigerhost/entry.py
@@ -2,7 +2,7 @@ import click
 
 import tigerhost
 
-from tigerhost.commands import apps, config, domains, keys
+from tigerhost.commands import apps, config, domains, git, keys
 from tigerhost.commands.user import login, user_info, logout
 from tigerhost.private_dir import ensure_private_dir_exists
 
@@ -30,6 +30,8 @@ entry.add_command(config.unset_config, name='config:unset')
 entry.add_command(domains.list_domains, name='domains')
 entry.add_command(domains.add_domain, name='domains:add')
 entry.add_command(domains.remove_domain, name='domains:remove')
+
+entry.add_command(git.add_remote, name='git:remote')
 
 entry.add_command(keys.add_key, name='keys:add')
 entry.add_command(keys.list_keys, name='keys')
