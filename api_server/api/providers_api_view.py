5a40eae088d2b40a4ad727560318676e4c796e32
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-01 13:19:36 -0500

    updated providers API to also return the default provider

--- api_server/api/providers_api_view.py
+++ api_server/api/providers_api_view.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.utils.decorators import method_decorator
 
 from api_server.api.api_base_view import ApiBaseView
@@ -10,8 +11,20 @@ class ProvidersApiView(ApiBaseView):
     def get(self, request):
         """Get the list of providers that this user has access to.
 
+        Return format (JSON):
+        {
+            'providers': ['provider1', 'provider2', ...]
+            'default': 'provider1'
+        }
+
         @type request: django.http.HttpRequest
 
         @rtype: django.http.HttpResponse
         """
-        return self.respond_multiple(request.user.profile.get_providers())
+
+        # TODO how do we ensure that the default provider is in the list of
+        # providers?
+        return self.respond({
+            'providers': request.user.profile.get_providers(),
+            'default': settings.DEFAULT_PAAS_PROVIDER,
+        })
--- api_server/tests/unit_tests/api/test_providers_api_view.py
+++ api_server/tests/unit_tests/api/test_providers_api_view.py
@@ -2,7 +2,8 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_GET(client, http_headers, user):
+def test_GET(client, http_headers, user, settings):
     resp = client.get('/api/v1/providers/', **http_headers)
     assert resp.status_code == 200
-    assert resp.json()['results'] == user.profile.get_providers()
+    assert resp.json()['providers'] == user.profile.get_providers()
+    assert resp.json()['default'] == settings.DEFAULT_PAAS_PROVIDER
