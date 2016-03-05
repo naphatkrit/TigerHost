8d947d0b6d4272166a71be292dd4fa3b6c35c532
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-05 10:24:51 -0500

    custom django command to get wsse secret

---
+++ wsse/management/commands/get_wsse_secret.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from wsse.utils import get_secret
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('usernames', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        for username in options['usernames']:
+            self.stdout.write('{username}: {token}\n'.format(username=username, token=get_secret(username)))
