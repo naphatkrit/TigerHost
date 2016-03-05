30d2a7a219233e7dd090f5a83fd091c88f581e4f
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-05 10:32:27 -0500

    updated help text for wsse command

--- wsse/management/commands/get_wsse_secret.py
+++ wsse/management/commands/get_wsse_secret.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from wsse.utils import get_secret
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Get the WSSE token for the specified users.'
 
     def add_arguments(self, parser):
         parser.add_argument('usernames', nargs='+', type=str)
