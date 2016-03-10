a6eb5df06ceb097ad1ecad8b4fa5b6b4e2fc5c40
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-10 08:37:12 -0500

    started an empty app aws_db_addons

--- settings/dev.py
+++ settings/dev.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aws_db_addons',
     'cas',
     'wsse',
     'api_server',
