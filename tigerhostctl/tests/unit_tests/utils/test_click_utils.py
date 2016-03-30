d20188856795472b94da94cda29b28c503b747a7
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-29 21:07:13 -0400

    [tigerhostctl] click utils to prompt for choices

---
+++ tigerhostctl/tests/unit_tests/utils/test_click_utils.py
@@ -0,0 +1,26 @@
+import click
+import pytest
+
+from tigerhost import exit_codes
+
+from tigerhostctl.utils import click_utils
+
+
+@click.command()
+def dummy():
+    value = click_utils.prompt_choices(['choice 1', 'choice 2', 'choice 3'])
+    click.echo('Choice is {}'.format(value + 1))
+
+
+@pytest.mark.parametrize('choice', range(1, 4))
+def test_choice_valid(choice, runner):
+    result = runner.invoke(dummy, input='{}\n'.format(choice))
+    assert result.exit_code == exit_codes.SUCCESS
+    assert 'Choice is {}'.format(choice) in result.output
+
+
+@pytest.mark.parametrize('choice', [0, 4])
+def test_choice_invalid(choice, runner):
+    result = runner.invoke(dummy, input='{}\n'.format(choice))
+    assert result.exit_code == exit_codes.ABORT
+    assert 'Invalid choice' in result.output
---
+++ tigerhostctl/tigerhostctl/utils/click_utils.py
@@ -0,0 +1,21 @@
+import click
+
+
+def prompt_choices(choices):
+    """Displays a prompt for the given choices
+
+    @type choices: list
+
+    @rtype: int
+        the index of the chosen choice
+    """
+    assert len(choices) > 1
+    for i in range(len(choices)):
+        click.echo('{number}) {choice}'.format(
+            number=i + 1,
+            choice=choices[i]
+        ))
+    value = click.prompt('1-{}'.format(len(choices)), type=int) - 1
+    if value < 0 or value >= len(choices):
+        raise click.ClickException('Invalid choice.')
+    return value
