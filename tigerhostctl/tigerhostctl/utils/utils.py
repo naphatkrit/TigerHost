4263e7dd539fe8a3204a55f7f5d60da5798afc15
Naphat Sanguansin <naphat.krit@gmail.com>
2016-03-31 12:03:50 -0400

    [tigerhostctl] a shell script parser for env variables

---
+++ tigerhostctl/tigerhostctl/utils/utils.py
@@ -0,0 +1,28 @@
+def parse_shell_for_exports(text):
+    """Parses a shell script and extracts all of its exports.
+
+    @type text: str
+        The shell script
+
+    @rtype: dict
+        for example,
+
+        export VAR1=value1
+        ...
+        export VAR2=value2
+
+        will return:
+
+        {
+            'VAR1': 'value1',
+            'VAR2': 'value2',
+        }
+    """
+    lines = [x for x in text.split('\n') if x.startswith('export ')]
+    lines = [x[len('export '):] for x in lines]
+    env = dict()
+    for l in lines:
+        key, value = l.split('=', 1)
+        value = value.split('#', 1)[0].strip()
+        env[key] = value
+    return env
