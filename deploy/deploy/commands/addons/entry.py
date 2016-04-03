5d65612c507b2392a99fea5b79a5b9dbadfe210a
Naphat Sanguansin <naphat.krit@gmail.com>
2016-04-03 15:27:51 -0400

    [deploy] addons update command

--- deploy/deploy/commands/addons/entry.py
+++ deploy/deploy/commands/addons/entry.py
@@ -3,6 +3,7 @@ import click
 from deploy.commands.addons.copy_credentials import copy_credentials
 from deploy.commands.addons.create import create
 from deploy.commands.addons.destroy import destroy
+from deploy.commands.addons.update import update
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -16,3 +17,4 @@ def entry():
 entry.add_command(copy_credentials)
 entry.add_command(create)
 entry.add_command(destroy)
+entry.add_command(update)
---
+++ deploy/deploy/commands/addons/update.py
@@ -0,0 +1,39 @@
+import click
+import os
+import subprocess32 as subprocess
+
+from tigerhost.utils.click_utils import echo_with_markers
+from tigerhost.utils.decorators import print_markers
+
+from deploy import settings
+from deploy.commands.addons.create import _generate_compose_file
+from deploy.project import get_project_path
+from deploy.secret import store
+from deploy.utils import utils
+from deploy.utils.decorators import ensure_project_path, require_docker_machine
+
+
+@click.command()
+@click.option('--name', '-n', default='tigerhost-addons-aws', help='The name of the machine to create. Defaults to tigerhost-addons-aws.')
+@print_markers
+@ensure_project_path
+@require_docker_machine
+def update(name):
+    echo_with_markers('Retrieving addons config.', marker='-')
+    database = store.get('addon__database_container_name', default=False)
+    if database is False:
+        raise click.exceptions.ClickException('Addons config not found. Was an addon server created with `{} addons create`?'.format(settings.APP_NAME))
+    click.echo('Done.')
+
+    project_path = get_project_path()
+    echo_with_markers('Generating docker-compose file.', marker='-')
+    _generate_compose_file(project_path, database)
+    click.echo('Done.')
+
+    echo_with_markers('Updating addons proxy.', marker='-')
+    env_text = subprocess.check_output(['docker-machine', 'env', name])
+    env = os.environ.copy()
+    env.update(utils.parse_shell_for_exports(env_text))
+
+    subprocess.check_call(['docker-compose', '-f', os.path.join(
+        project_path, 'proxy/docker-compose.prod.yml'), 'up', '-d'], env=env)
