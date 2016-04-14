689399c3a2bbddc58b9f61da6817e977e7d18875
Naphat Sanguansin <naphat.krit@gmail.com>
2016-04-14 14:50:49 -0400

    [web] validate the input to env variables to make sure it doesn't contain characters that can cause issues with Deis

--- web/api_server/api/app_env_variables_api_view.py
+++ web/api_server/api/app_env_variables_api_view.py
@@ -1,10 +1,16 @@
 import json
+import re
 
 from django.utils.decorators import method_decorator
 
-from api_server.api.api_base_view import ApiBaseView
+from api_server.api.api_base_view import ApiBaseView, ErrorResponse
 from api_server.paas_backends import get_backend_authenticated_client
 from wsse.decorators import check_wsse_token
+
+
+_valid_chars = r'[0-9a-zA-Z_\-@:/+.]'
+
+_value_regexp = re.compile(r'^{}*$'.format(_valid_chars))
 
 
 @method_decorator(check_wsse_token, 'dispatch')
@@ -46,6 +52,10 @@ class AppEnvVariablesApiView(ApiBaseView):
         backend = self.get_backend_for_app(app_id)
         auth_client = get_backend_authenticated_client(
             request.user.username, backend)
+
+        for v in env_vars.itervalues():
+            if v is not None and not _value_regexp.match(v):
+                raise ErrorResponse(message='Variable values must be one of {}. Invalid character(s) in {}'.format(_valid_chars, v), status=400)
 
         auth_client.set_application_env_variables(app_id, env_vars)
         return self.respond()
--- web/api_server/tests/unit_tests/api/test_app_env_variables_api_view.py
+++ web/api_server/tests/unit_tests/api/test_app_env_variables_api_view.py
@@ -24,7 +24,12 @@ def test_GET(client, http_headers, mock_backend_authenticated_client, app_id, ma
 
 
 @pytest.mark.django_db
-def test_POST(client, http_headers, mock_backend_authenticated_client, app_id, make_app):
+@pytest.mark.parametrize('value', [
+    'admin@example.com',
+    'admin+123@example.com',
+    'postgres://user:pass@host:1234/testing',
+])
+def test_POST(value, client, http_headers, mock_backend_authenticated_client, app_id, make_app):
     """
     @type client: django.test.Client
     @type http_headers: dict
@@ -33,9 +38,37 @@ def test_POST(client, http_headers, mock_backend_authenticated_client, app_id, m
     bindings = {
         'VAR1': 'value1',
         'VAR2': 'value2',
+        'VAR3': value,
     }
     with mock.patch('api_server.api.app_env_variables_api_view.get_backend_authenticated_client') as mocked:
         mocked.return_value = mock_backend_authenticated_client
         resp = client.post('/api/v1/apps/{}/env/'.format(app_id), data=json.dumps(bindings), content_type='application/json', **http_headers)
     assert resp.status_code == 204
     mock_backend_authenticated_client.set_application_env_variables.assert_called_once_with(app_id, bindings)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('bad_value', [
+    '\\',
+    '|',
+    'testing:#@gmail.com',
+    'testing:%@gmail.com',
+    'testing:!@gmail.com',
+])
+def test_POST_invalid(bad_value, client, http_headers, mock_backend_authenticated_client, app_id, make_app):
+    """
+    @type client: django.test.Client
+    @type http_headers: dict
+    @type mock_backend_authenticated_client: mock.Mock
+    """
+    bindings = {
+        'VAR1': 'value1',
+        'VAR2': 'value2',
+        'VAR3': bad_value
+    }
+    with mock.patch('api_server.api.app_env_variables_api_view.get_backend_authenticated_client') as mocked:
+        mocked.return_value = mock_backend_authenticated_client
+        resp = client.post('/api/v1/apps/{}/env/'.format(app_id), data=json.dumps(bindings), content_type='application/json', **http_headers)
+    assert resp.status_code == 400
+    assert bad_value in resp.json()['error']
+    assert mock_backend_authenticated_client.set_application_env_variables.call_count == 0
